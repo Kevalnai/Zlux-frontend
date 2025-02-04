7621fab45e49153e6a855b30761862cf4d3fca07
Keval Nai <kevalnai1504@gmail.com>
2025-02-04 02:37:38 -0500

    Working branch

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import Navbar from "../components/Navbar";  // ✅ Import Navbar
+import HeroSection from "../components/HeroSection"; // ✅ Import HeroSection
+import Body from "../components/Body"; // ✅ Import Body content
+import Footer from "../components/Footer"; // ✅ Import Footer
 
-function Home(){
-
-    return(
+function Home() {
+    return (
         <>
-            <div className="home">
-            <Navbar />
-            <Body />
-            </div>
+            <Navbar /> {/* Include Navbar */}
+            <Body /> {/* Main content */}
+
         </>
     );
 };
 
 export default Home;
-
-
